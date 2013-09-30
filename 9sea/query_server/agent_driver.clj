995b407c633d1d0c94a3d3548784a524973b48da
zhangjun <zhangjun.julian@gmail.com>
2013-09-30 17:10:12 +0800

    create table

--- 9sea/query_server/agent_driver.clj
+++ 9sea/query_server/agent_driver.clj
@@ -1,13 +1,15 @@
 (ns query-server.agent-driver
     (:require 
         [clj-http.client :as client]
+        [query-server.query-backend :as qb]
+        [query-server.hive-adapt :as ha]
     )
 )
 
 (def ^:private urlmap
     {
-        :get-setting ""
-        :get-schema ""
+        :get-setting "/get-setting"
+        :get-schema "/get-schemas"
     }
 )
 
@@ -22,8 +24,10 @@
 
 (defn- gen-table-list [accountid appname appversion dbsc]
     (let [dbname (:dbname dbsc)
-            tablelist (map)
+            tbl (:tables dbsc)
+            tns (str accountid "/" appname "/" appversion "/" dbname)
         ]
+        (map #(assoc % :namespace (str tns "." (:tablename %) )) tbl)
     )
 )
 
@@ -33,14 +37,47 @@
             appname (:app setting)
             appversion (:appversion setting)
             schema (httpget agenturl :get-schema)
-            dblist (flatten (map 
+            datalist (flatten (map 
                     (partial gen-table-list accountid appname appversion) 
                     schema 
                 ))
         ]
+        (map create-table datalist)
     )
 )
 
+(defn- runsql [sql]
+                (jdbc/with-connection qb/my-db
+                    (jdbc/with-query-results res [sql]
+                        (->>
+                            res
+                            doall
+                        )
+                    )
+                )
+)
+
+(defn- check-table [tns]
+    (let [sql (str "select * from TblMetaStore where namespace = '" tns "'")
+            rcount (count   (runsql sql))
+        ]
+        (cond
+            (<= 1 rcount) true
+            :else false
+        )
+    )
+
+)
+
+(defn create-table [dataset]
+    (when (check-table (:namespace dataset))
+        (ha/create-table (:namespace dataset) (:cols dataset))
+    )
+)
+
+(defn -main []
+    (new-agent "11" "dfdsf" "http://192.168.1.101:8082" "sdfdsf")
+)
 
 
 
