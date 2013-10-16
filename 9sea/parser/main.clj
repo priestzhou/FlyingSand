b4e1c42dd2981e6998d1780c11f54df5af17ac82
taoda <tyf@flying-sand.com>
2013-10-16 10:43:46 +0800

    sql parser

---
+++ 9sea/parser/main.clj
@@ -0,0 +1,6 @@
+(ns parser.main
+    (:gen-class)
+)
+
+(defn -main [& args]
+)
---
+++ 9sea/parser_unittest/parser/translator.clj
@@ -0,0 +1,320 @@
+(ns parser-unittest.parser.translator
+    (:use 
+        [testing.core :only (suite)]
+    )
+    (:require
+        [clojure.string :as str]
+        [utilities.parse :as prs]
+        [parser.translator :as trans]
+    )
+    (:import
+        [utilities.parse InvalidSyntaxException]
+    )
+)
+
+(defn name-conversion [nm]
+    (format "`%s`"
+        (str/join "." nm)
+    )
+)
+
+(def context {
+    :ns [{
+        :type "namespace"
+        :name "com"
+        :children [{
+            :type "namespace"
+            :name "app"
+            :children [{
+                :type "namespace"
+                :name "ver"
+                :children [{
+                    :type "table"
+                    :name "tbl"
+                    :children [
+                        {
+                            :name "col"
+                            :type "varchar(255)"
+                        }
+                    ]
+                }]
+            }]
+        }]
+    }]
+    :default-ns ["com" "app" "ver"]
+    :name-conversion name-conversion
+})
+
+(suite "quoted"
+    (:fact quoted
+        (trans/quoted \` "ab")
+        :is
+        "ab"
+    )
+    (:fact quoted:special-char
+        (trans/quoted \` "a+b")
+        :is
+        "`a+b`"
+    )
+    (:fact quoted:escaped
+        (trans/quoted \` "a`b")
+        :is
+        "`a``b`"
+    )
+)
+
+(suite "normalize-table"
+    (:fact normalize-table:only-table
+        (trans/normalize-table context ["tbl"])
+        :is
+        ["com" "app" "ver" "tbl"]
+    )
+    (:fact normalize-table:namespaced-table
+        (trans/normalize-table context ["ver" "tbl"])
+        :is
+        ["com" "app" "ver" "tbl"]
+    )
+    (:fact normalize-table:absolute
+        (trans/normalize-table context ["com" "app" "ver" "tbl"])
+        :is
+        ["com" "app" "ver" "tbl"]
+    )
+    (:fact normalize-table:unmatch
+        (fn []
+            (trans/normalize-table context ["lbt"])
+        )
+        :throws InvalidSyntaxException
+    )
+)
+
+(suite "sql->hive"
+    (:fact to-hive
+        (trans/sql-2003->hive context "SELECT * FROM tbl")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:as
+        (trans/sql-2003->hive context "SELECT * FROM (SELECT * FROM tbl) AS t")
+        :is
+        "SELECT * FROM (SELECT * FROM `com.app.ver.tbl` tbl) t"
+    )
+    (:fact to-hive:select-column:raw-table
+        (trans/sql-2003->hive context "SELECT col FROM tbl")
+        :is
+        "SELECT col FROM `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:select-column:raw-table:selected
+        (trans/sql-2003->hive context "SELECT tbl.col FROM tbl")
+        :is
+        "SELECT tbl.col FROM `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:select-column:derived-table
+        (trans/sql-2003->hive context "SELECT col FROM tbl AS tbl")
+        :is
+        "SELECT col FROM `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:where
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE col<1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE col < 1"
+    )
+    (:fact to-hive:group-by
+        (trans/sql-2003->hive context "SELECT * FROM tbl GROUP BY col, 1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl GROUP BY col, 1"
+    )
+    (:fact to-hive:order-by
+        (trans/sql-2003->hive context "SELECT * FROM tbl ORDER BY col, col ASC, 1 DESC")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl ORDER BY col, col ASC, 1 DESC"
+    )
+    (:fact to-hive:limit
+        (trans/sql-2003->hive context "SELECT * FROM tbl LIMIT 1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl LIMIT 1"
+    )
+    (:fact to-hive:union:all
+        (trans/sql-2003->hive context "SELECT * FROM tbl UNION ALL SELECT * FROM tbl")
+        :is
+        "(SELECT * FROM `com.app.ver.tbl` tbl) UNION ALL (SELECT * FROM `com.app.ver.tbl` tbl)"
+    )
+    (:fact to-hive:cross-join
+        (trans/sql-2003->hive context "SELECT * FROM tbl CROSS JOIN tbl")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl CROSS JOIN `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:cross-join:on
+        (trans/sql-2003->hive context "SELECT * FROM tbl t1 CROSS JOIN tbl t2 ON t1.col=t2.col")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 CROSS JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col)"
+    )
+    (:fact to-hive:inner-join
+        (trans/sql-2003->hive context "SELECT * FROM tbl JOIN tbl")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl JOIN `com.app.ver.tbl` tbl"
+    )
+    (:fact to-hive:inner-join:on
+        (trans/sql-2003->hive context "SELECT * FROM (tbl t1) JOIN (tbl t2) ON t1.col=t2.col")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col)"
+    )
+    (:fact to-hive:join-after-join
+        (trans/sql-2003->hive context 
+            "SELECT * FROM (tbl t1) JOIN (tbl t2) ON t1.col=t2.col JOIN (tbl t3) ON t3.col=t2.col"
+        )
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col) JOIN `com.app.ver.tbl` t3 ON (t3.col = t2.col)"
+    )
+    (:fact to-hive:outer-join:left
+        (trans/sql-2003->hive context 
+            "SELECT * FROM (tbl t1) LEFT JOIN (tbl t2) ON t1.col=t2.col"
+        )
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 LEFT JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col)"
+    )
+    (:fact to-hive:outer-join:right
+        (trans/sql-2003->hive context 
+            "SELECT * FROM (tbl t1) RIGHT JOIN (tbl t2) ON t1.col=t2.col"
+        )
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 RIGHT JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col)"
+    )
+    (:fact to-hive:outer-join:full
+        (trans/sql-2003->hive context 
+            "SELECT * FROM (tbl t1) FULL JOIN (tbl t2) ON t1.col=t2.col"
+        )
+        :is
+        "SELECT * FROM `com.app.ver.tbl` t1 FULL JOIN `com.app.ver.tbl` t2 ON (t1.col = t2.col)"
+    )
+    (:fact to-hive:where:precedence
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE TRUE OR NOT FALSE AND UNKNOWN")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE TRUE OR (NOT FALSE) AND UNKNOWN"
+    )
+    (:fact to-hive:where:test
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE TRUE IS TRUE")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE TRUE IS TRUE"
+    )
+    (:fact to-hive:where:test:not
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE TRUE IS NOT FALSE")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE TRUE IS NOT FALSE"
+    )
+    (:fact to-hive:where:test:chain
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE FALSE IS NOT NULL IS TRUE")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE (FALSE IS NOT NULL) IS TRUE"
+    )
+    (:fact to-hive:where:test:null
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE TRUE IS NULL")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE TRUE IS NULL"
+    )
+    (:fact to-hive:where:=
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1=1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 = 1"
+    )
+    (:fact to-hive:where:comparison:<=>
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1<=>1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 <=> 1"
+    )
+    (:fact to-hive:where:comparison:!=
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1!=1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 <> 1"
+    )
+    (:fact to-hive:where:comparison:<>
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1<>1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 <> 1"
+    )
+    (:fact to-hive:where:comparison:<
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1<1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 < 1"
+    )
+    (:fact to-hive:where:comparison:<=
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1<=1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 <= 1"
+    )
+    (:fact to-hive:where:comparison:>
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1>1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 > 1"
+    )
+    (:fact to-hive:where:comparison:>=
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1>=1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 >= 1"
+    )
+    (:fact to-hive:where:bit:|
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1|1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 | 1"
+    )
+    (:fact to-hive:where:bit:&
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1&1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 & 1"
+    )
+    (:fact to-hive:where:bit:<<
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1<<1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 << 1"
+    )
+    (:fact to-hive:where:bit:>>
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1>>1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 >> 1"
+    )
+    (:fact to-hive:where:bit:caret
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1^1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 ^ 1"
+    )
+    (:fact to-hive:where:bit:unary-tilde
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE ~ 1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE ~ 1"
+    )
+    (:fact to-hive:where:arithmetic:+
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1+1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 + 1"
+    )
+    (:fact to-hive:where:arithmetic:-
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1-1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 - 1"
+    )
+    (:fact to-hive:where:arithmetic:*
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1*1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 * 1"
+    )
+    (:fact to-hive:where:arithmetic:div
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1/1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 / 1"
+    )
+    (:fact to-hive:where:arithmetic:%
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE 1%1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE 1 % 1"
+    )
+    (:fact to-hive:where:arithmetic:unary+
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE + 1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE + 1"
+    )
+    (:fact to-hive:where:arithmetic:unary-
+        (trans/sql-2003->hive context "SELECT * FROM tbl WHERE - 1")
+        :is
+        "SELECT * FROM `com.app.ver.tbl` tbl WHERE - 1"
+    )
+)
+
