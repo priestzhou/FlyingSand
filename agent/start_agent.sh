27138f95cb0b7daa3f1d534c1571d3506b4db846
zhangjun <zhangjun.julian@gmail.com>
2013-10-21 17:37:22 +0800

    change shell

--- agent/start_agent.sh
+++ agent/start_agent.sh
@@ -1,1 +1,3 @@
-nohup ./jre1.7.0_40/bin/java -cp .:agent.jar agent.main -webport 8081 2>&1 >>agent.log & 
+pw=`pwd`
+jpath=$pw/jre1.7.0_40/bin/java
+JAVA_HOME=$jpath nohup $jpath -cp .:agent.jar agent.main -webport 8081 2>&1 >>agent.log & 
--- agent/start_monitor.sh
+++ agent/start_monitor.sh
@@ -1,1 +1,3 @@
-nohup ./jre1.7.0_40/bin/java -cp .:monitor.jar monitor.main -webport 8082 2>&1 >> monitor.log & 
+pw=`pwd`
+jpath=$pw/jre1.7.0_40/bin/java
+JAVA_HOME=$jpath nohup $jpath -cp .:monitor.jar monitor.main -webport 8082 2>&1 >> monitor.log & 
