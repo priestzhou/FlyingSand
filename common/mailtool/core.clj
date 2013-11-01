6ed4621459c3d8dfe9b458e315f3de586f708db0
zhangjun <zhangjun.julian@gmail.com>
2013-11-01 17:10:44 +0800

    demo worked

---
+++ common/mailtool/core.clj
@@ -0,0 +1,25 @@
+(ns mailtool.core
+    (:require
+        [postal.core :as pc]
+    )
+)
+(defn send-html-mail [to subject htmlbody]
+    (pc/send-message 
+        ^{:host "smtp.exmail.qq.com"
+            :user "admin@flying-sand.com"
+            :pass "fs123456"
+        }
+        {:from "admin@flying-sand.com"
+        :to to
+        :subject subject
+        :body 
+            [:alternative
+                {:type "text/html"
+                    :content htmlbody
+                }
+            ]
+        }
+
+    )
+)
+;" java -cp  .:monitor.jar monitor.mian "
---
+++ common/mailtool/main.clj
@@ -0,0 +1,15 @@
+(ns mailtool.main
+    (:require
+        [mailtool.core :as mc]
+    )
+    (:gen-class)
+)
+
+
+(defn -main []
+    (let [
+            _ (println "test")
+        ]
+    )
+)
+;" java -cp  .:monitor.jar monitor.mian "
