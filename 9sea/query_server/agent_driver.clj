21ef5f1aeb89b99dd401566f39aa095348f955e8
zhangjun <zhangjun.julian@gmail.com>
2013-11-07 16:52:45 +0800

    fix

--- 9sea/query_server/agent_driver.clj
+++ 9sea/query_server/agent_driver.clj
@@ -352,7 +352,7 @@
 )
 
 (defn- updata-agent-sync-time [agentid]
-    (let [now (coe/unparse (tformat/formatters :date-hour-minute-second) (time/now))
+    (let [now (tformat/unparse (tformat/formatters :date-hour-minute-second) (time/now))
             _ (debug "updatetime" now)
             sql (str "update TblAgent set LastSyncTime=\"" now "\" "
                     " where id =" agentid
