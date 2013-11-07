003aada8d9ab8684d1d4618ed2f4e7d16237c384
admin <admin@flyingsand00.flyingsand>
2013-11-07 16:27:21 +0800

    refine code structure

--- 9sea/query_server/core.clj
+++ 9sea/query_server/core.clj
@@ -240,14 +240,16 @@
         (= view-or-ctas :view)
         (do
           (hive/create-view hive-name hql)
-          (orm/insert mysql/TblMetaStore (orm/values [{:NameSpace ns :AppName (:appname context-info) :AppVersion (:appversion context-info)
+          (orm/insert mysql/TblMetaStore 
+                      (orm/values [{:NameSpace ns :AppName (:appname context-info) :AppVersion (:appversion context-info)
                                              :DBName (:database context-info) :TableName (:tablename context-info) 
                                              :hive_name (:hive-name context-info) :NameSpaceType 3}]))
         )
         (= view-or-ctas :ctas)
         (do
           (hive/create-CTAS hive-name hql)
-          (orm/insert mysql/TblMetaStore (orm/values [{:NameSpace ns :AppName (:appname context-info) :AppVersion (:appversion context-info)
+          (orm/insert mysql/TblMetaStore 
+                      (orm/values [{:NameSpace ns :AppName (:appname context-info) :AppVersion (:appversion context-info)
                                              :DBName (:database context-info) :TableName (:tablename context-info) 
                                              :hive_name hive-name :NameSpaceType 2}]))
         )
@@ -293,7 +295,8 @@
    )
   (catch SQLException sql-ex
     (error "can't drop view/table" (util/except->str sql-ex))
-    (update-result-map q-id "failed" nil (str "can't drop view/table " (:hive-name context-info) (.getMessage sql-ex)) nil)
+    (update-result-map 
+      q-id "failed" nil (str "can't drop view/table " (:hive-name context-info) (.getMessage sql-ex)) nil)
   ))
 ) 
 
