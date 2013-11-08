cef067f86b50701f2de0483245f93a64f6737c9d
admin <admin@flyingsand00.flyingsand>
2013-11-08 15:49:22 +0800

    fix duplicate hive db

--- 9sea/query_server/hive_adapt.clj
+++ 9sea/query_server/hive_adapt.clj
@@ -43,28 +43,13 @@
 (def ^:private hive-conn-str (ref ""))
 
 (def ^:private table-type ["" "table" "ctas" "view"])
-
-(defn set-hive-db
-  [host port]
-  (dosync
-    (alter hive-db conj {:subname (format "//%s:%s" host port)})
-    (ref-set hive-conn-str (format "jdbc:hive://%s:%s" host port))
-  )
-  (prn "hive-db" @hive-db)
-  (prn "hive-conn-str" @hive-conn-str)
-)
-
-(defn get-hive-db
-  []
-  (-> @hive-db)
-)
+(def ^:private hive-conn-str (ref ""))
 
 (defn get-hive-conn-str
   []
   (-> @hive-conn-str)
 )
 
-(def ^:private hive-conn-str (ref ""))
 
 (def ^:private table-type ["" "table" "ctas" "view"])
 
